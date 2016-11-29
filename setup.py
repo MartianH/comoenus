02673fbe6e576398e300d36332c0512a9d5d8dbb
MartianH <martial.habimana@gmail.com>
2016-11-29 01:10:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,5 @@ from setuptools import setup
 
 
 setup(
-    name='comoenus',
-    install_requires=[
-                    'flask==0.10.1',
-                    'flask-sqlalchemy==2.1',
-                    'flask-bcrypt==0.7.1',
-                    'flask-script==2.0.5',
-                    'flask-wtf==0.12',
-                    'mysql-python==1.2.5',
-                    'pyjade==4.0.0',
-                    'python-slugify==1.2.0'
-                    'behave'
-                    ]
+    name='comoenus'
 )
